0513d09b882b54194352f2649fa3e9ef472e7f12
Lokesh <147271484+lokesh2910v@users.noreply.github.com>
2025-05-27 01:15:57 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -56,13 +56,13 @@ function Login() {
           <p className="text-blue-100">Sign in to access your dashboard</p>
 
   <h3 style="margin-top: 0;">Demo Accounts for Testing</h3>
-  <p><strong>Manager Account</strong></br>
-     Username: <code>manager2004@gmail.com</code></br>
-     Password: <code>12345</code>
+  <p>Manager Account
+     Username: manager2004@gmail.com
+     Password: 12345
   </p>
-  <p><strong>Employee Account</strong></br>
-     Username: <code>employee2004@gmail.com</code></br>
-     Password: <code>12345</code>
+  <p>Employee Account
+     Username: employee2004@gmail.com
+     Password: 12345
   </p>
 
 
